503d955dcc7e8a4f0475b21fb4a53b247cc75c9b
sathya00-CodeZero <opsathya00@gmail.com>
2025-12-03 13:26:45 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -90,6 +90,13 @@ const handleCustomEvent = () => {
               <span onClick={handleCustomEvent}>Get Started</span>
             </Link>
           </div>
+          <div id="Resul-banner-ads"
+     banner-props='{
+       "bannerName": "Testing_index_1",
+       "width": 600,
+       "height": 350
+     }'>
+</div>
         </div>
         <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
